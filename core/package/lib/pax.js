638c4cfe6a5c5afebb893a7068f6d6a6eba1d406
James Struga <jstruga@rocketsoftware.com>
2025-02-07 10:01:41 -0500

    clear cache
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>

--- core/package/lib/pax.js
+++ core/package/lib/pax.js
@@ -96,7 +96,7 @@ tar xpoUf ../../zlux.tar
 cd zlux-server-framework 
 rm -rf node_modules 
 export NODE_HOME=${maristNode}
-_TAG_REDIR_ERR=txt _TAG_REDIR_IN=txt _TAG_REDIR_OUT=txt __UNTAGGED_READ_MODE=V6 PATH=${maristNode}/bin:.:/bin npm --verbose install 
+_TAG_REDIR_ERR=txt _TAG_REDIR_IN=txt _TAG_REDIR_OUT=txt __UNTAGGED_READ_MODE=V6 PATH=${maristNode}/bin:.:/bin npm --verbose install --cache /ZOWE/tmp/.npm
 cd .. 
 iconv -f iso8859-1 -t 1047 zlux-app-server/defaults/serverConfig/server.json > zlux-app-server/defaults/serverConfig/server.json.1047 
 mv zlux-app-server/defaults/serverConfig/server.json.1047 zlux-app-server/defaults/serverConfig/server.json 
